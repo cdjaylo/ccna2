ee912c8c9fe687216da73e0a4ff879be1f5d5ace
cdjaylo <christiandavejaylo@gmail.com>
2024-05-20 16:16:37 +0800

    Add files via upload

---
+++ dns.ps1
@@ -0,0 +1,18 @@
+﻿Add-DnsServerPrimaryZone -Name "ngcp61.ph" -ZoneFile "ngcp61.ph.dns"
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name ns -ipv4address 10.61.1.10
+add-DnsServerResourceRecord -zonename ngcp61.ph -Cname -name www -hostname ns.ngcp61.ph
+add-DnsServerResourceRecord -zonename ngcp61.ph -Cname -name imap -hostname ns.ngcp61.ph
+add-DnsServerResourceRecord -zonename ngcp61.ph -Cname -name pop -hostname ns.ngcp61.ph
+add-DnsServerResourceRecord -zonename ngcp61.ph -Cname -name smtp -hostname ns.ngcp61.ph
+###FOR CISCO DEVICES DNS RECORDS:
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name cb -ipv4address 10.61.1.4
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name ct -ipv4address 10.61.1.2
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name cm -ipv4address 10.61.100.8
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name ed -ipv4address 10.61.61.1
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name p1 -ipv4address 10.61.100.101
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name p2 -ipv4address 10.61.100.102
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name c1 -ipv4address 10.61.50.6
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name c2 -ipv4address 10.61.50.8
+add-DnsServerResourceRecord -zonename ngcp61.ph -A -name ap -ipv4address 10.61.10.3
+
+
---
+++ ngcp61website.ps1
@@ -0,0 +1,2 @@
+﻿Install-WindowsFeature  -name Web-Server  -includeManagementTools
+New-Website -name "ngcp61.ph" -hostheader "www.ngcp61.ph" -physicalpath "D:\webs\datingbiz" -Force
